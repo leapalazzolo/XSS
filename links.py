0a6ea59866f69380ba10b5a26ac0cdb8bfc40171
leapalazzolo <leapalazzolo@gmail.com>
2017-10-02 23:59:59 -0300

    Testear links

--- links.py
+++ links.py
@@ -1,21 +1,77 @@
 import os
 import sys
 import re
+import httplib
 import urllib2, urllib
 import urlparse
 import mechanize
 from bs4 import BeautifulSoup, SoupStrainer
 
+blacklist = ['.png', '.jpg', '.jpeg', '.mp3', '.mp4', '.gif', '.svg',
+             '.pdf', '.doc', '.docx', '.zip', '.rar', '.rss']#, '.js', '.min.js']
+             #TODO ver que los iltimos no sean esos
 
-def obtener_scripts_desde_url(url, esquema, link_a_archivo, link_a_dominio_o_subdominio, cookies=None):
-    br = mechanize.Browser()
-    _configurar_navegador(br)
-    if not abrir_url_en_navegador(br, url ,cookies):
-        print "Error al abrir la URL.", url
-        if cookies is not None:
-            print "Revise las cookies.", cookies
-        return list()
-    html = br.response().read()
+class Links(object):
+    '''
+    '''
+    def __init__(self, url_):
+        self._url = url_
+        self._esquema = str()
+        self._scripts = list()
+        self._entradas = dict()
+        self._html = str()
+        self._parametros = dict()
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def scripts(self):
+        return self._scripts
+    
+    @scripts.setter
+    def scripts(self, script):
+        self._scripts.append(script)
+
+    @property
+    def html(self):
+        return self._html
+    
+    @html.setter
+    def html(self, html):
+        self._html = html
+
+    @property    
+    def entradas(self):
+        return self.entradas
+    
+    @entradas.setter
+    def entradas(self, entradas):
+        self._entradas = entradas
+    
+    @property    
+    def parametros(self):
+        return self.parametros
+    
+    @parametros.setter
+    def entradas(self, parametros):
+        self._parametros = parametros
+
+def obtener_parametros_de_la_url(query):
+    parametros = urlparse.parse_qs(query,keep_blank_values=True)
+
+
+
+def obtener_scripts_desde_url(url, esquema, html, link_a_archivo, link_a_dominio_o_subdominio, cookies=None):#, cookies=None):
+    #br = mechanize.Browser()
+    #configurar_navegador(br)
+    #if not abrir_url_en_navegador(br, url ,cookies):
+    #    print "Error al abrir la URL.", url
+    #    if cookies is not None:
+    #        print "Revise las cookies.", cookies
+    #    return list()
+    #html = br.response().read()
     bs = BeautifulSoup(html, 'lxml')
     scripts = set()
     for script in bs.findAll('script'):
@@ -24,29 +80,76 @@ def obtener_scripts_desde_url(url, esquema, link_a_archivo, link_a_dominio_o_sub
         if texto != '':
             #print texto.encode("utf-8") #TODO enconde para todos los archivos
             scripts.add(texto)
+            #print texto[0:5]
         else:
             if script.has_attr('src'):
                 if link_a_archivo.match(script['src']) is not None:
-                    link_script = urlparse.urljoin(url, script['src'])
-                    #print link_script
-                    script_js =  urllib2.urlopen(link_script).read()
-                    scripts.add(script_js)
-                else:
-                    if link_a_dominio_o_subdominio.match(script['src']) is not None:
-                        #print 'Mismo dominio o subdominio: ', link.url
-                        link_script = script['src']
-                        if link_script[0] == '/' and link_script[1] == '/':
-                            link_script.replace('//', esquema + '://')
+                    try:
+                        link_script = urlparse.urljoin(url, script['src'])
                         #print link_script
                         script_js =  urllib2.urlopen(link_script).read()
                         scripts.add(script_js)
+                    except urllib2.HTTPError, error:
+                        print error, url
+                        pass
+                else:
+                    if link_a_dominio_o_subdominio.match(script['src']) is not None:
+                        try: #print 'Mismo dominio o subdominio: ', link.url
+                            link_script = script['src']
+                            if link_script[0] == '/' and link_script[1] == '/':
+                                link_script = link_script.replace('//', esquema + '://')
+                            #print link_script
+                            script_js =  urllib2.urlopen(link_script).read()
+                            scripts.add(script_js)
+                        except urllib2.HTTPError, error:
+                            print error, url
+                            pass
         #os.system('pause')
     return list(scripts)
 
-def _configurar_navegador(browser):
+
+def obtener_entradas_desde_url(br, html):
+    nro_form = 0
+    forms_y_parametros = dict()
+    bs = BeautifulSoup(html, 'lxml')
+
+    #try:
+    #    forms = br.forms()
+    #except (ValueError, mechanize._form.ParseError) as error:
+    #    print error
+    #    return forms_y_parametros TODO hidden
+    listform = ['radio', 'checkbox','text', 'password', 'email', 'url', 'search', 'hidden', '']
+
+    for form in bs.findAll('form'):  
+        #br.select_form(nr=nro_form)    # submit the first form
+        #controls = form.controls
+        lista_parametros =  form.findAll('input', {'type':''}) + form.findAll('input', {'type':listform}) + form.findAll('textarea')
+        #for parametro in form.findAll('input'), {'type':'text'}:
+            #if '<Text' or '<Password' or '<Check' or '<Radio' or '<Select' in str(parametro): #TODO ver esto
+            #print parametro.name, parametro.type, parametro.id
+            #if 'TextControl' in str(parametro) or 'TextareaControl' in str(parametro):
+        #    lista_parametros.append(parametro)
+        #    print parametro['name']
+        #print lista_parametros
+        if lista_parametros:
+            forms_y_parametros[nro_form] = lista_parametros
+        nro_form += 1
+    return forms_y_parametros
+
+def configurar_navegador(browser):
     browser.addheaders = [('User-agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)Gecko/20071127 Firefox/2.0.0.11')]
     browser.set_handle_robots(False)
     browser.set_handle_refresh(False)
+    #browser.add_handler(PrettifyHandler()) TODO VER ACA
+    #browser.add_handler(PrettifyHandler())
+    #browser.set_handle_redirect(True) #TODO VEr aca
+
+def es_url_prohibida(url):
+    for extension in blacklist:
+        if extension in url:
+            return True
+    return False
+
 
 def es_url_valida(url):
     """Devuelve si una url se encuentra bien formada y existe conexion hacia ella.
@@ -58,8 +161,9 @@ def es_url_valida(url):
     True
     """
     url_parseado = urlparse.urlparse(url)
+    #root, ext = os.path.splitext(url_parseado.path)
     #request = requests.get(url)
-    return all([url_parseado.scheme, url_parseado.netloc])
+    return all([url_parseado.scheme, url_parseado.netloc, not es_url_prohibida(url)])
 
 def se_puede_acceder_a_url(url):
     """Devuelve si en una url existe conexion hacia ella.
@@ -95,78 +199,134 @@ def abrir_url_en_navegador(br, url, cookies=None):
                 br.open(url) #TODO: try except
             conectado = True # if line above fails, this is never executed
             return conectado
-        except mechanize.HTTPError as e:
-            print e.code            
+        except (mechanize.HTTPError,mechanize.URLError, urllib2.URLError) as error:
+            print 'URL invalida', error            
             intentos += 1        
-            if intentos > 4:
-                return False
-        except mechanize.URLError as e:
-            print e.reason.args            
-            intentos += 1
             if intentos > 4:
                 return False
 
 def obtener_links_validos_desde_url(url, cookies=None):
-    br = mechanize.Browser()
-    _configurar_navegador(br)
-   #cookies = ["JSESSIONID=08DA6EBFBB3136C6C893DAC963C19682.nodo01_t02", "SRV=nodo01_t02"]
-    #   br.set_cookie(cookie)
-    scripts = set()
+    br = mechanize.Browser()#factory=mechanize.DefaultFactory(i_want_broken_xhtml_support=True))
+    configurar_navegador(br)
+    if not abrir_url_en_navegador(br, url ,cookies) or not es_url_valida(url):
+        print "Error al abrir la URL."
+        if cookies is not None:
+            print "Revise las cookies."
+        return list()
+    
+    #cookies = ["JSESSIONID=76E51C2E3C449FB79D5F032071E89D19.nodo01_t01", "SRV=nodo01_t01"]
+    #br.set_cookie(cookie)
     url_parseado = urlparse.urlparse(url)
-    url_y_scripts = dict()
+    #root, extension = os.path.splitext(url_parseado)
+    #url_y_scripts = dict()
     regex_link_a_archivo = '(?!^//)[A-Za-z0-9_\-//]*\.\w*'  #TODO ver lo de r'
-    regex_mismo_dominio_o_subdominio =  r'.*\b' + url_parseado.hostname.replace('www.','.?') + r'\b(?!\.)' # OK? Lo del igual es para evitar un x.com/?a=y.com y aggara el ultimo
+    regex_mismo_dominio_o_subdominio =  r'.*\b' + url_parseado.hostname.replace('www.','\.?') + r'\b(?!\.)' # OK? Lo del igual es para evitar un x.com/?a=y.com y aggara el ultimo
     regex_link_a_carpeta = '(?!^//)[A-Za-z0-9_\-//]*/[A-Za-z0-9_\-\.//]*'
     
     link_a_archivo = re.compile(regex_link_a_archivo)        # Archvos con su ruta
     link_a_carpeta = re.compile(regex_link_a_carpeta)        # OK  TODO
     link_a_dominio_o_subdominio = re.compile(regex_mismo_dominio_o_subdominio)
-    if not abrir_url_en_navegador(br, url ,cookies):
-        print "Error al abrir la URL."
-        if cookies is not None:
-            print "Revise las cookies."
-        return list()
     #obtener_scripts_desde_html(html, url, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
     #os.system('pause')  
-    links_validos = set()
-    links_validos.add(url)
+    #print url
+    links = list()
+    #url = url if url[-1] == '/' else url +'/'
+    links.append(url)
     for link in br.links():
+        links.append(link.url)
+    #links_validos = set()
+    #print links
+    links_utiles = dict()
+    for link in links:
+        #print link
         link_valido = str()
-        if link_a_archivo.match(link.url) is not None or link_a_carpeta.match(link.url) is not None:
-            link_valido = urlparse.urljoin(url, link.url)
-            if es_url_valida(link_valido):
+        #print link #TODO TODO TODO ver el / al final de la url para que no repita
+        #TODO TODO TODO TODO Filtrar urls antes
+        if link_a_archivo.match(link) is not None or link_a_carpeta.match(link) is not None: #br.links() y link.url
+            link_valido = urlparse.urljoin(url, link)
             #print link_valido
+            if '#' in link_valido:
+                link_valido = link_valido[:link_valido.index('#')]
+                #print '#'
+            #link_valido = link_valido if link_valido[-1] == '/' else link_valido +'/'
+            if es_url_valida(link_valido):
                 #links_validos.add(link_valido)
-                if link_valido not in url_y_scripts: #TODO no es necesario, pero por si falla lo anterior
-                    url_y_scripts[link_valido] = obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
+                if link_valido not in links_utiles: #TODO no es necesario, pero por si falla lo anterior
+                    
+                    print link_valido
+                    if abrir_url_en_navegador(br, link_valido):
+                        objeto_link = Links(link_valido)
+                        try:
+                            objeto_link.html = br.response().read()#obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
+                        except httplib.IncompleteRead as error:
+                            print error, link_valido
+                        objeto_link.scripts = obtener_scripts_desde_url(link_valido, url_parseado.scheme, objeto_link.html, link_a_archivo, link_a_dominio_o_subdominio)
+                        objeto_link.entradas = obtener_entradas_desde_url(br, objeto_link.html)
+                        links_utiles[link_valido] = objeto_link
         else:
-            if link_a_dominio_o_subdominio.match(link.url) is not None:
-                #print 'Mismo dominio o subdominio: ', link.url
-                link_valido = link.url
+            if link_a_dominio_o_subdominio.match(link) is not None:
+                #print 'Mismo dominio o subdominio: ', link
+                #link_valido = link if link[-1] == '/' else link + '/'
+                link_valido = link
                 if link_valido[0] == '/' and link_valido[1] == '/':
                     link_valido.replace('//', url_parseado.scheme + '://') # TODO test 
+                    #link_valido = link_valido[:link_valido.index('#')] if '#' in link_valido else link_valido
+                if '#' in link_valido:
+                    link_valido = link_valido[:link_valido.index('#')] + '/'
+                    #print '#'
                 if es_url_valida(link_valido):
                     #links_validos.add(link_valido)
-                    if link_valido not in url_y_scripts:
-                        url_y_scripts[link_valido] = obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
+                    print link_valido
+                    if link_valido not in links_utiles: #TODO no es necesario, pero por si falla lo anterior
+                        #print link_valido
+                        if abrir_url_en_navegador(br, link_valido):
+                            #urls_y_html[link_valido] = br.response().read()
+                            #print link_valido
+                            objeto_link = Links(link_valido)
+                            #TODO excep abajo
+                            try:
+                                objeto_link.html = br.response().read()#obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
+                            except httplib.IncompleteRead as error:
+                                print error, link_valido
+                            
+                            #objeto_link.html = br.response().read()#obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
+                            objeto_link.scripts = obtener_scripts_desde_url(link_valido, url_parseado.scheme, objeto_link.html, link_a_archivo, link_a_dominio_o_subdominio)
+                            objeto_link.entradas = obtener_entradas_desde_url(br, objeto_link.html)
+                            links_utiles[link_valido] = objeto_link
+                            #print urls_y_html[link_valido]
+                    #if link_valido not in url_y_scripts:
+                    #    url_y_scripts[link_valido] = obtener_scripts_desde_url(link_valido, url_parseado.scheme, link_a_archivo, link_a_dominio_o_subdominio)
     #print url_y_scripts
     #lista_links_validos = list(links_validos)
     #lista_links_validos.sort()
     #print lista_links_validos
-    #for link in url_y_scripts:
-    #    print link
-    #    os.system('pause')
-    #    for script in url_y_scripts[link]:
-    #        print "\t", script
-    #        os.system('pause')
-    return url_y_scripts
+    #archivo = open('test.txt', 'w+')
+    print len(links_utiles)
+    for link in links_utiles:
+        #print link
+        #archivo.write(links_utiles[link].html)
+        #print links_utiles[link].html
+        #print links_utiles[link].scripts
+        #archivo.write(links_utiles[link].scripts)
+        #archivo.write(links_utiles[link].parametros)
+        params = links_utiles[link].parametros
+        for form in params:
+            print form
+            for param in params[form]:
+                print param
+        #archivo.write(links_utiles[link].url)
+        print links_utiles[link].url
+        os.system('pause')
+    #archivo.close()
+    #print urls_y_html
+    return links_utiles
+    #return list(link_validos)
 
 if __name__ == '__main__':
     url = sys.argv[1]
     if not es_url_valida(url):
         sys.exit("Esquema de URL invalido. Ingrese nuevamente.")
     if not se_puede_acceder_a_url(url):
-        pass
-        #sys.exit("No se puede acceder a la URL. Revise la conexion o el sitio web.")
+        sys.exit("No se puede acceder a la URL. Revise la conexion o el sitio web.")
     obtener_links_validos_desde_url(sys.argv[1])
     #obtener_scripts_desde_url(sys.argv[1])
--- xss.py
+++ xss.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import codecs
 import links
 import mechanize
 import selenium
@@ -10,31 +11,86 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 blacklist = ['.png', '.jpg', '.jpeg', '.mp3', '.mp4', '.gif', '.svg',
-             '.pdf', '.doc', '.docx', '.zip', '.rar']
+             '.pdf', '.doc', '.docx', '.zip', '.rar', '.rss']
 
-payloads = ['<script>alert(1)</script>', '<IMG SRC=/ onerror="alert(String.fromCharCode(88,83,83))"></img>', 'javascript:alert(1)']
+#payloads = ['<script>alert(1)</script>', '<IMG SRC=/ onerror="alert(String.fromCharCode(88,83,83))"></img>', 'javascript:alert(1)']
 
 #DOM_FILTER_REGEX = r"(?s)<!--.*?-->|\bescape\([^)]+\)|\([^)]+==[^(]+\)|\"[^\"]+\"|'[^']+'"
 
-DOM_SOURCES_RE = re.compile("""/(location\s*[\[.])|([.\[]\s*["']?\s*(arguments|dialogArguments|innerHTML|write(ln)?|open(Dialog)?|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)\W)|(localStorage|sessionStorage|Database)/""")
-DOM_SINKS_RE = re.compile("""/((src|href|data|location|code|value|action)\s*["'\]]*\s*\+?\s*=)|((replace|assign|navigate|getResponseHeader|open(Dialog)?|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\s*["'\]]*\s*\()/""")   
+re_domxss_sources = re.compile('(location\s*[\[.])|([.\[]\s*["\']?\s*(arguments|dialogArguments|innerHTML|write(ln)?|open(Dialog)?|showModalDialog|cookie|URL|documentURI|baseURI|referrer|name|opener|parent|top|content|self|frames)\W)|(localStorage|sessionStorage|Database)')
+re_domxss_sinks = re.compile('((src|href|data|location|code|value|action)\s*["\'\]]*\s*\+?\s*=)|((replace|assign|navigate|getResponseHeader|open(Dialog)?|showModalDialog|eval|evaluate|execCommand|execScript|setTimeout|setInterval)\s*["\'\]]*\s*\()')
+
 #TODO singleton
 
+def detectar_WAF():
+    noise = "<script>alert()</script>" #a payload which is noisy enough to provoke the WAF
+    fuzz = URL.replace("d3v", noise) #Replaces "d3v" in url with noise
+    res1 = urlopen(fuzz) #Opens the noise injected payload
+    if res1.code == 406 or res1.code == 501: #if the http response code is 406/501
+        print"\033[1;31m[-]\033[1;m WAF Detected : Mod_Security"
+        print "\033[1;33m[!]\033[1;m Delaying requests to avoid WAF detection\n"
+        WAF = "True" #A WAF is present
+        waf_choice()
+    elif res1.code == 999: #if the http response code is 999
+        print"\033[1;31m[-]\033[1;m WAF Detected : WebKnight"
+        print "\033[1;33m[!]\033[1;m Delaying requests to avoid WAF detection\n"
+        WAF = "True"
+        waf_choice()
+    elif res1.code == 419: #if the http response code is 419
+        print"\033[1;31m[-]\033[1;m WAF Detected : F5 BIG IP"
+        print "\033[1;33m[!]\033[1;m Delaying requests to avoid WAF detection\n"
+        WAF = "True"
+        waf_choice()
+    elif res1.code == 403: #if the http response code is 403
+        print "\033[1;31m[-]\033[1;m Unknown WAF Detected"
+        print "\033[1;33m[!]\033[1;m Delaying requests to avoid WAF detection\n"
+        WAF = "True"
+        waf_choice()
+    elif res1.code == 302: #if redirection is enabled
+        print "\033[1;31m[-]\033[1;m Redirection Detected! Exploitation attempts may fail.\n"
+        choice()
+    else:
+        print "\033[1;32m[+]\033[1;m WAF Status: Offline\n"
+        WAF = "False" #No WAF is present
+        choice()
 
-def detectar_xss_DOM(script):
+def cargar_payloads():
+    lista_payloads = []
+    try:
+        with open('payloads.txt', 'r') as archivo_payloads:
+            for linea in archivo_payloads.readlines():
+                lista_payloads.append(linea)
+                #print linea
+    except EnvironmentError, error:
+        print error
+    finally:
+        #print len(lista_payloads)
+        return lista_payloads
+
+
+def detectar_xss_DOM(scripts):
     nro_linea = 0
-    for linea in script.split('\n'):
-        nro_linea += 1
-        if DOM_SOURCES_RE.match(linea) or DOM_SINKS_RE.match(linea):
-            return nro_linea
+    for script in scripts:
+        for linea in script.split('\n'):
+            nro_linea += 1
+            patron = re_domxss_sources.search(linea) 
+            #for grupo in patron.groups():
+            if patron is not None:
+                print 'DOM XSS source',linea 
+                #os.system('pause')
+            patron = re_domxss_sinks.search(linea)
+            if patron is not None:
+                print 'DOM XSS sink', linea
+                #os.system('pause')
     return 0
 
 def detectar_xss_reflejado(br, payload):
     if payload in br.response().read(): #TODO: XSS reflejado
-        print "DOM XSS"
+        os.system('pause')
         #br.back()
         return True
     #br.back()
@@ -66,42 +122,41 @@ def detectar_xss_almacenado(driver, parametro, payload):
     except:
         return False
     finally:
-        driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
+        driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
         #driver.execute_script("window.history.go(-1)")
         #return True
-    
-
 def inyectar_payload(br, payload, parametro): #NO hace falta determinar si es get o post.
     br.form[parametro.name] = payload
     br.submit()
 
-def buscar_vulnerabilidad_xss(url, scripts, cookies=None):
+def buscar_vulnerabilidad_xss(url_ingresada, lista_payloads, cookies=None):
+
     for extension in blacklist:
-        if extension in url:
+        if extension in url_ingresada:
             return False
     
     #driver = webdriver.Firefox()
     #driver.get(url)
     #if cookies is not None:
     #    driver.manage().addCookie(cookies)
-    br = mechanize.Browser()  # initiating the browser
+    br = mechanize.Browser(factory=mechanize.RobustFactory())  # factory=mechanize.RobustFactory()
     br.addheaders = [('User-agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)Gecko/20071127 Firefox/2.0.0.11')]
     br.set_handle_robots(False)
     br.set_handle_refresh(False)
 
     driver = webdriver.Firefox()    #PhantomJS 
-    driver.get(url)
+    driver.get(url_ingresada)
     if cookies is not None:
         driver.manage().addCookie(cookies)
     
-    if not links.abrir_url_en_navegador(br, url, cookies):
+    if not links.abrir_url_en_navegador(br, url_ingresada, cookies):
         print "Error al abrir la URL."
         if cookies is not None:
             print "Revise las cookies."
         return False
 
     i = 0
-    
+    forms_y_controles = dict()
     forms = br.forms() 
     for form in forms:     # if a form exists, submit it
         #params = form#list(br.forms())[0]    # our form
@@ -111,14 +166,15 @@ def buscar_vulnerabilidad_xss(url, scripts, cookies=None):
             #print str(parametro)
             #par = str(p)
             # submit only those forms which require text
+            print parametro.name, parametro.type, parametro.id
             if 'TextControl' in str(parametro) or 'TextareaControl' in str(parametro):
-                #print parametro.name, parametro.type, parametro.id
                 p = 0
                 encontrado = False
-                while p < len(payloads) and not encontrado:
-                    #print parametro.name
+                while p < len(lista_payloads) and not encontrado:
+                    #print parametro.type
                     #os.system('pause')
-                    payload = payloads[p]
+                    payload = lista_payloads.pop(p)
+                    print payload
                     inyectar_payload(br, payload, parametro)
                     if detectar_xss_reflejado(br, payload):
                         #TODO log, bdd, etc.
@@ -131,9 +187,13 @@ def buscar_vulnerabilidad_xss(url, scripts, cookies=None):
                         else:
                             print "Reflejado"   #XSS encontrado solo al inyectar payload 
                     else:
-                        input = driver.find_element_by_id(parametro.id)
-                        if detectar_xss_almacenado(driver, input, payload):
-                            encontrado = True
+                        try:
+                            entrada = driver.find_element_by_id(parametro.id)
+                            if detectar_xss_almacenado(driver, entrada, payload):
+                                encontrado = True
+                        except NoSuchElementException as no_existe_elemento:
+                            print no_existe_elemento
+                            pass
                         br.back()
                         br.select_form(nr=i)
                         #TODO: setup.py con selenium
@@ -146,18 +206,28 @@ def buscar_vulnerabilidad_xss(url, scripts, cookies=None):
 
         i += 1
         #print i
+    br.close()
+    driver.quit()
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    if not links.es_url_valida(url): #TODO: chequear url antes de la lista o cuando se va a hacer lo de xss?
+    #url = sys.argv[1]
+    payloads = cargar_payloads()
+    if payloads is None:
+        sys.exit('Error al cargar las payloads.Revise el archivo.')
+    if payloads == []:
+        sys.exit("No se encontraron paylods. Revise el archivo 'payloads.txt' en la carpeta raiz.")
+    if not links.es_url_valida(sys.argv[1]): #TODO: chequear url antes de la lista o cuando se va a hacer lo de xss?
         sys.exit("Esquema de URL invalido. Ingrese nuevamente.")
-    if not links.se_puede_acceder_a_url(url):
+    if not links.se_puede_acceder_a_url(sys.argv[1]):
         sys.exit("No se puede acceder a la URL. Revise la conexion o el sitio web.")
-    urls_y_scripts = links.obtener_links_validos_desde_url(sys.argv[1])
+    print sys.argv[1]
+    links = links.obtener_links_validos_desde_url(sys.argv[1])
     
     #for url in urls_encontradas:
-    for url in urls_y_scripts:
-        buscar_vulnerabilidad_xss(url, urls_y_scripts[url])
-        #detectar_xss_almacenado(url)
-
+    for url in links:
+        #print url
+        link = links[url]
+        detectar_xss_DOM(link.scripts)
+        buscar_vulnerabilidad_xss(url, payloads)
+        
 
